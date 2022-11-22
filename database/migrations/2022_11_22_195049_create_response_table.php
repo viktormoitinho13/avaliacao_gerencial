7eaa7ae79463e46989caf8262b3357c3764e62ee
viktorpromofarma <viktor.santos@promofarma.com.br>
2022-11-22 17:16:15 -0300

    Create table questions and response

---
+++ database/migrations/2022_11_22_195049_create_response_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResponseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('response', function (Blueprint $table) {
+            $table->id('id');
+            $table->bigInteger('id_questions');
+             $table->foreign('id_questions')->references('id')->on('questions');  
+             $table->string('response');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('response');
+    }
+}
