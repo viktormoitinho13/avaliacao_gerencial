2c1af1b6a70c3e1da176f61fba6ada8f1c0dd49c
viktorpromofarma <viktor.santos@promofarma.com.br>
2022-11-30 00:16:52 -0300

    Create card in home

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,8 @@
 <section class="vh-100 mt-5">
   <div class="container-fluid h-custom">
     <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-md-9 col-lg-6 col-xl-5">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-          class="img-fluid" alt="Sample image">
+      <div class="col-md-9 col-lg-6 col-xl-5 ml-5">
+       <img src="{{URL::asset('/imgs/capa.jpg')}}" alt="profile Pic"   class="img-fluid" alt="Sample image" >
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
      
@@ -22,13 +21,13 @@
             <div>
                  <!-- Email input -->
          <div class="col-md-6 ">
-                  <label class="form-label" for="login">Nome Completo ou  Matrícula</label>
+                  <label class="form-label" for="login">Seu Nome Completo ou Sua Matrícula</label>
                   <input class="form-control form-control-lg" type="text" id="login" name="login"><br><br>
             </div>
 
             <!-- Password -->
-            <div class="col-md-6 mb-4">
-                <x-label for="password" :value="__('Password')" class="form-label"/>
+            <div class="col-md-6 mb-4 mt-2">
+                <x-label for="password" :value="__('Sua Senha')" class="form-label"/>
 
                 <x-input id="password" class="form-label" type="password" name="password" required
                     autocomplete="current-password" class="form-control form-control-lg" />
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,21 @@
 <x-app-layout>
 
+
+
+<div class="container-fluid">
+<div class="row mt-5">
  @foreach ($classificacoes as $classificacao)
-<p>{{ $classificacao->AG_CLASSIFICACAO }} </p>
-<p>{{ $classificacao->CLASSIFICACAO }} </p>
+  <div class="col-sm-3">
+     <div class="card text-center w-75 mx-auto mb-1 mt-5 ">
+    <img src="{{URL::asset('/imgs/capa.jpg')}}" alt="profile Pic"   class="img-fluid" alt="Sample image"  class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">{{ $classificacao->CLASSIFICACAO }}</h5>
+       
+        <a href="/form/{{ $classificacao->AG_CLASSIFICACAO }}" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
 @endforeach
-
-
-
+</div>
+</div>
 </x-app-layout>
