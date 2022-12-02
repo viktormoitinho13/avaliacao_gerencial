b42dfe1e32ffa9b9bde5614ab8abff6f353f2473
viktorpromofarma <viktor.santos@promofarma.com.br>
2022-12-02 12:20:48 -0300

    Add icons in forms

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,8 @@
       <img src="{{URL::asset('/imgs/capa.jpg')}}" alt="profile Pic"   class="img-fluid" alt="Sample image"  class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">{{ $classificacao->CLASSIFICACAO }}</h5>
-         <a href="/form/{{ $classificacao->AG_CLASSIFICACAO }}" class="btn" style="background-color: #6B9DD8; color:white;">Ir ao questionário</a>
+        
+         <a href="/form/{{ $classificacao->AG_CLASSIFICACAO }}" class="btn btn-primary px-3" style="background-color: #6B9DD8; color:white;"><i class="fas fa-hand-point-right" aria-hidden="true"></i>  Ir ao questionário</a>
         
       </div>
     </div>
