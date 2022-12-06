9a9db1c3ea6122972ce45aa057ec799135a616bb
viktorpromofarma <viktor.santos@promofarma.com.br>
2022-12-06 17:59:29 -0300

    Alter hash from password

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,7 +42,7 @@ class RegisteredUserController extends Controller
               $user = User::create([
             'name' => $request->name,
             'registration' => $request->registration,
-            'password' => Hash::make($request->password),
+           'password' => Hash::make($request->password),
         ]);
 
         event(new Registered($user));
