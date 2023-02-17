947d736b510b8e085b4ace66b9e13b2c5aced73c
viktorpromofarma <viktor.santos@promofarma.com.br>
2023-02-17 17:04:23 -0300

    tudo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 
 <nav x-data="{ open: false }" class="navbar navbar-expand-lg navbar-dark " style="background-color: #6B9DD8;">
   <div class="container-fluid">
-    <a href="/" class="navbar-brand mx-3"> <img src="{{URL::asset('/imgs/índice2.png')}}"  class="img-fluid"  class="card-img-top" height="100" width="30"> Olá, {{ strstr(Auth::user()->name ,' ',true) }}</a> 
+    <a href="{{ route('home')}} " class="navbar-brand mx-3"> <img src="{{URL::asset('/imgs/índice2.png')}}"  class="img-fluid"  class="card-img-top" height="100" width="30"> Olá, {{ strstr(Auth::user()->name ,' ',true) }}</a> 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
